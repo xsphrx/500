97b63c116362b46f3f2a9485d7d47b286f66ee47
vict <przemek.maranowski@gmail.com>
2020-05-25 16:50:55 +0200

    update

--- war.py
+++ war.py
@@ -1,26 +1,13 @@
-import sys, pygame
+import sys
+import os
+import pygame
 import random
+import time
 
 red = (255, 0, 0)
 black = (0, 0, 0)
 suits = (('♥', red), ('♣', black), ('♦', red),('♠', black))
 ranks = {11:'J', 12:'Q', 13:'K', 14:'A'}
-test = 'awe'
-
-class Button(pygame.sprite.Sprite):
-    def __init__(self, text):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((100,30))
-        self.rect = self.image.get_rect()
-        self.rect.center = (x,y)
-        self.text = text
-        self.active = 1
-
-        self.image.fill((255,255,255))
-        pygame.draw.rect(self.image, (0,0,0), (0,0,100,30), 1)
-        n = font_option.render(self.text, True, (0,0,0))
-        text_rect = n.get_rect(center=(self.image.get_width()/2, self.image.get_height()/2))
-        self.image.blit(n, text_rect)
 
 
 class Card(pygame.sprite.Sprite):
@@ -60,39 +47,44 @@ class Card(pygame.sprite.Sprite):
         else:
             self.image.fill((255,255,255))
 
-            if self.rank > 10:
-                self.rank = ranks[self.rank]
-            n = font.render(str(self.rank), True, (0,0,0))
-            b = font.render(suits[self.suit-1][0], True, (0,0,0))
+            if self.rank > 10: cur_rank = ranks[self.rank]
+            else: cur_rank = self.rank
+            n = font.render(str(cur_rank), True, (0,0,0))
+
+            b = font.render(suits[self.suit-1][0], True, suits[self.suit-1][1])
             self.image.blit(n, (0,0))
             self.image.blit(b, (0,25))
 
         pygame.draw.rect(self.image, (0,0,0), (0,0,50,70), 1)
         self.up = not self.up
 
+    def flip_down(self):
+        self.image.fill((169,169,169))
+        pygame.draw.rect(self.image, (0,0,0), (0,0,50,70), 1)
+        self.up = 0
 
 
-def new_round():
-    # random card from player's and enemy's deck is picked moved and flipped
-    player_card = player.pop(random.randrange(len(player)))
-    enemy_card = enemy.pop(random.randrange(len(enemy)))
-
-    player_card.move_to(300, 400)
-    enemy_card.move_to(300, 200)
-
-    player_card.flip()
-    enemy_card.flip()
-
-    message[0] = 'wer'
+def rearrange_cards():
+    for i, x in enumerate(player):
+        cards.change_layer(x,35+i)
+        x.move_to(600, 400-i*0.5)
+    for i, x in enumerate(enemy):
+        cards.change_layer(x,35+i)
+        x.move_to(600, 200-i*0.5)
+    for i, x in enumerate(player_card):
+        cards.change_layer(x,35+i)
+        x.move_to(300+20*i, 400)
+    for i, x in enumerate(enemy_card):
+        cards.change_layer(x,35+i)
+        x.move_to(300+20*i, 200)
 
 
 def main():
     pygame.init()
     pygame.font.init()
 
-    global font,font_option, screen
+    global font, screen
     font = pygame.font.SysFont("arial", 22)
-    font_option = pygame.font.SysFont("arial", 14)
 
     screen = pygame.display.set_mode((800, 600))
 
@@ -120,12 +112,18 @@ def main():
         player.append(card1)
         enemy.append(card2)
 
+    global cards, player_card, enemy_card
     cards = pygame.sprite.LayeredUpdates(player, enemy)
 
-    global message
-    message = ['abc']
+    player_card = []
+    enemy_card = []
 
-    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    global message
+    message = ['']
+
+    message_font = pygame.font.SysFont('Comic Sans MS', 30)
+
+    rearrange_cards()
 
     while True:
         screen.blit(background, (0, 0))
@@ -136,10 +134,73 @@ def main():
                 clicked_sprites = [s for s in cards if s.rect.collidepoint(pos)]
                 clicked_card = next(iter(clicked_sprites), None)
                 if clicked_card in player:
-                    start_round()
+                    if message[0] == 'its a draw':
 
-        textsurface = myfont.render(message[0], False, (0, 0, 0))
-        screen.blit(textsurface,(0,0))
+                        player_card.append(player.pop(random.randrange(len(player))))
+                        enemy_card.append(enemy.pop(random.randrange(len(enemy))))
+                        player_card.append(player.pop(random.randrange(len(player))))
+                        enemy_card.append(enemy.pop(random.randrange(len(enemy))))
+                        player_card[-1].flip()
+                        enemy_card[-1].flip()
+
+                        rearrange_cards()
+
+                        if player_card[-1].rank > enemy_card[-1].rank:
+                            message[0] = 'player wins'
+
+                        elif player_card[-1].rank < enemy_card[-1].rank:
+                            message[0] = 'enemy wins'
+                        else:
+                            message[0] = 'its a draw'
+                        break
+
+
+
+                    if player_card:
+                        for x in player_card: x.flip_down()
+                        for x in enemy_card: x.flip_down()
+                        if message[0] == 'player wins':
+                            player += player_card
+                            player += enemy_card
+                            player_card = []
+                            enemy_card = []
+                        elif message[0] == 'enemy wins':
+                            enemy += player_card
+                            enemy += enemy_card
+                            player_card = []
+                            enemy_card = []
+
+
+
+
+                    # random card from player's and enemy's deck is picked moved and flipped
+                    player_card.append(player.pop(random.randrange(len(player))))
+                    enemy_card.append(enemy.pop(random.randrange(len(enemy))))
+
+
+
+                    player_card[0].flip()
+                    enemy_card[0].flip()
+
+                    rearrange_cards()
+
+                    if player_card[0].rank > enemy_card[0].rank:
+                        message[0] = 'player wins'
+
+                    elif player_card[0].rank < enemy_card[0].rank:
+                        message[0] = 'enemy wins'
+
+                    else:
+                        message[0] = 'its a draw'
+
+
+        textsurface = message_font.render('enemy', False, (0, 0, 0))
+        screen.blit(textsurface,(100,200))
+        textsurface = message_font.render('player', False, (0, 0, 0))
+        screen.blit(textsurface,(100,400))
+
+        textsurface = message_font.render(message[0], False, (0, 0, 0))
+        screen.blit(textsurface,(400,100))
 
         cards.draw(screen)
         pygame.display.flip()
